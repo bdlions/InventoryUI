16ed4ef99388a0f9422d97f9733fc614b759b347
Alamgir Kabir <alamgirkabir7@gmail.com>
2017-09-25 13:38:13 +0600

    add title bar design

---
+++ resources/js/title-bar.js
@@ -0,0 +1,39 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function () {
+  
+  const remote = require('electron').remote;
+  
+  function init() {
+    document.getElementById("min-btn").addEventListener("click", function (e) {
+      const window = remote.getCurrentWindow();
+      window.minimize();
+    });
+    
+    document.getElementById("max-btn").addEventListener("click", function (e) {
+      const window = remote.getCurrentWindow();
+      if (!window.isMaximized()) {
+        window.maximize();
+      } else {
+        window.unmaximize();
+        }
+      });
+      
+      document.getElementById("close-btn").addEventListener("click", function (e) {
+        const window = remote.getCurrentWindow();
+        window.close();
+      });
+    };
+    
+    document.onreadystatechange = function () {
+      if (document.readyState == "complete") {
+        init();
+      }
+    };
+          
+})();

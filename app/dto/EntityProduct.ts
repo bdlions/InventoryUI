0b8cca48019f200065f5faccab4d4443bf0dd87c
nazmulhasan <bdlions@gmail.com>
2017-09-26 11:49:03 +0600

    product info is updated

--- app/dto/EntityProduct.ts
+++ app/dto/EntityProduct.ts
@@ -1,6 +1,7 @@
 export class EntityProduct {
     id: number;
     categoryId: number;
+    categoryTitle: string;
     name: string;
     code: string;
     width: string;
@@ -9,6 +10,7 @@ export class EntityProduct {
     unitPrice: number;
     length: string;
     typeId: number;
+    typeTitle: string;
     standardUOMId: number;
     saleUOMId: number;
     purchaseUOMId: number;

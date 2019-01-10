77ac1574011713809739c7188e5a7376e8124893
nazmulhasan <bdlions@gmail.com>
2019-01-11 03:06:15 +0600

    search sale/purchase by customer/supplier

--- app/report/purchase/purchaseordersummary.component.ts
+++ app/report/purchase/purchaseordersummary.component.ts
@@ -3,6 +3,10 @@ import { DatePipe } from '@angular/common';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ModalDirective} from 'ngx-bootstrap';
 import {DTOPurchaseOrder} from './../../dto/DTOPurchaseOrder';
+import {EntityUser} from './../../dto/EntityUser';
+import {EntitySupplier} from "./../../dto/EntitySupplier";
+import {EntityUserRole} from "./../../dto/EntityUserRole";
+import {DTOSupplier} from './../../dto/DTOSupplier';
 import {WebAPIService} from './../../webservice/web-api-service';
 import {PacketHeaderFactory} from './../../webservice/PacketHeaderFactory';
 import {ACTION} from './../../webservice/ACTION';
@@ -20,6 +24,8 @@ export class PurchaseOrderSummary {
     private datePipe: DatePipe;
     private webAPIService: WebAPIService;
     private subscribe: Subscription;
+    
+    @ViewChild('purchaseOrderSummarySupplierModal') public purchaseOrderSummarySupplierModal: ModalDirective;
 
     public showStartDatePicker: boolean = false;
     public showEndDatePicker: boolean = false;
@@ -33,6 +39,11 @@ export class PurchaseOrderSummary {
     private purchaseOrderList: DTOPurchaseOrder[];
     private totalPurchaseAmount: number;
     
+    private reqDTOSupplier: DTOSupplier;
+    private supplierList: DTOSupplier[];
+    private dtoSupplier: DTOSupplier;    
+    private supplierRequestId: number;
+    
     private offset: number;
     private limit: number;
     
@@ -40,6 +51,10 @@ export class PurchaseOrderSummary {
     length = 0;
     pageSize = 10;
     pageSizeOptions = [5, 10];
+    
+    supplierLength = 0;
+    supplierPageSize = 10;
+    supplierPageSizeOptions = [5, 10];
 
     constructor(private router: Router, public route: ActivatedRoute, webAPIService: WebAPIService, private navigationManager: NavigationManager, public datepipe: DatePipe) {
         this.navigationManager.showNavBarEmitter.subscribe((mode) => {
@@ -55,11 +70,25 @@ export class PurchaseOrderSummary {
         this.webAPIService = webAPIService;
         this.datePipe = datepipe;
         
+        this.dtoSupplier = new DTOSupplier();
+        this.dtoSupplier.entitySupplier = new EntitySupplier();
+        this.dtoSupplier.entityUser = new EntityUser();
+        this.dtoSupplier.entityUserRole = new EntityUserRole();
+        
         this.totalPurchaseAmount = 0;
         this.offset = 0;
         this.limit = this.pageSize;
         this.purchaseOrderList = Array();
         this.fetchPurchaseOrderList();
+        
+        this.reqDTOSupplier = new DTOSupplier();
+        this.reqDTOSupplier.entitySupplier = new EntitySupplier();
+        this.reqDTOSupplier.entityUser = new EntityUser();
+        this.reqDTOSupplier.limit = this.supplierPageSize;
+        this.reqDTOSupplier.offset = 0;
+        this.supplierRequestId = ACTION.FETCH_SUPPLIERS;
+        this.fetchSupplierList();
+        
     }
 
     ngOnInit() {
@@ -70,7 +99,12 @@ export class PurchaseOrderSummary {
     public fetchPurchaseOrderList() {
         let fromDate: string = this.datepipe.transform(this.fromDate, 'yyyy-MM-dd');
         let toDate: string = this.datepipe.transform(this.toDate, 'yyyy-MM-dd');
-        let requestBody: string = "{\"startDate\": \"" + fromDate + "\", \"endDate\": \"" + toDate + "\", \"offset\": \"" + this.offset + "\", \"limit\": \"" + this.limit + "\"}";
+        let userId: number = -1;
+        if (this.dtoSupplier.entityUser.id > 0)
+        {
+            userId = this.dtoSupplier.entityUser.id;
+        }
+        let requestBody: string = "{\"startDate\": \"" + fromDate + "\", \"endDate\": \"" + toDate + "\", \"userId\": \"" + userId + "\", \"offset\": \"" + this.offset + "\", \"limit\": \"" + this.limit + "\"}";
         this.webAPIService.getResponse(PacketHeaderFactory.getHeader(ACTION.FETCH_PURCHASE_ORDER_SUMMARY), requestBody).then(result => {
             if (result.success && result.purchaseOrders != null) {
                 this.purchaseOrderList = result.purchaseOrders;
@@ -107,6 +141,116 @@ export class PurchaseOrderSummary {
     printPurchaseOrderSummary(event: Event) {
 
     }
+    
+    //---------------------------------supplier section starts ----------------------------------------//
+    public fetchSupplierList() {
+        let requestBody: string = JSON.stringify(this.reqDTOSupplier);
+        this.webAPIService.getResponse(PacketHeaderFactory.getHeader(ACTION.FETCH_SUPPLIERS), requestBody).then(result => {
+            if (result.success && result.suppliers != null) {
+                this.supplierList = result.suppliers;
+                this.supplierLength = result.totalSuppliers;
+            }
+        });
+    }
+    
+    public fetchSupplierListByName() {
+        let requestBody: string = JSON.stringify(this.reqDTOSupplier);
+        this.webAPIService.getResponse(PacketHeaderFactory.getHeader(ACTION.FETCH_SUPPLIERS_BY_NAME), requestBody).then(result => {
+            if (result.success && result.suppliers != null) {
+                this.supplierList = result.suppliers;
+                this.supplierLength = result.totalSuppliers;
+            }
+        });
+    }
+    
+    public fetchSupplierListByCell() {
+        let requestBody: string = JSON.stringify(this.reqDTOSupplier);
+        this.webAPIService.getResponse(PacketHeaderFactory.getHeader(ACTION.FETCH_SUPPLIERS_BY_CELL), requestBody).then(result => {
+            if (result.success && result.suppliers != null) {
+                this.supplierList = result.suppliers;
+                this.supplierLength = result.totalSuppliers;
+            }
+        });
+    }
+    
+    public fetchSupplierListByEmail() {
+        let requestBody: string = JSON.stringify(this.reqDTOSupplier);
+        this.webAPIService.getResponse(PacketHeaderFactory.getHeader(ACTION.FETCH_SUPPLIERS_BY_EMAIL), requestBody).then(result => {
+            if (result.success && result.suppliers != null) {
+                this.supplierList = result.suppliers;
+                this.supplierLength = result.totalSuppliers;
+            }
+        });
+    }
+
+    public hidePurchaseOrderSummarySupplierModal(): void {
+        this.purchaseOrderSummarySupplierModal.hide();
+    }
+
+    public showPurchaseOrderSummarySupplierModal(event: Event) {
+        this.purchaseOrderSummarySupplierModal.config.backdrop = false;
+        this.purchaseOrderSummarySupplierModal.show();
+    }
+
+    public searchPurchaseOrderSupplier(event: Event) {
+        this.reqDTOSupplier.limit = this.supplierPageSize;
+        this.reqDTOSupplier.offset = 0;
+        if (this.reqDTOSupplier.entitySupplier.supplierName != null && this.reqDTOSupplier.entitySupplier.supplierName != "")
+        {
+            this.supplierRequestId = ACTION.FETCH_SUPPLIERS_BY_NAME;
+            this.fetchSupplierListByName();
+        }
+        else if (this.reqDTOSupplier.entitySupplier.cell != null && this.reqDTOSupplier.entitySupplier.cell != "")
+        {
+            this.supplierRequestId = ACTION.FETCH_SUPPLIERS_BY_CELL;
+            this.fetchSupplierListByCell();
+        }
+        else if (this.reqDTOSupplier.entitySupplier.email != null && this.reqDTOSupplier.entitySupplier.email != "")
+        {
+            this.supplierRequestId = ACTION.FETCH_SUPPLIERS_BY_EMAIL;
+            this.fetchSupplierListByEmail();
+        }
+        else
+        {
+            //if nothing is set then get all suppliers
+            this.supplierRequestId = ACTION.FETCH_SUPPLIERS;
+            this.fetchSupplierList();
+        }        
+    }
+
+    public selectedSupplier(event: Event, supplierId: number) 
+    {
+        this.purchaseOrderSummarySupplierModal.hide();
+        let supplierCounter: number;
+        for (supplierCounter = 0; supplierCounter < this.supplierList.length; supplierCounter++) {
+            if (this.supplierList[supplierCounter].entitySupplier.id == supplierId) {
+                this.dtoSupplier = this.supplierList[supplierCounter];         
+            }
+        }
+    }
+    
+    onSupplierPaginateChange(event:PageEvent)
+    {
+        this.reqDTOSupplier.limit = event.pageSize;
+        this.reqDTOSupplier.offset = (event.pageIndex * event.pageSize) ;
+        if (this.supplierRequestId == ACTION.FETCH_SUPPLIERS)
+        {
+            this.fetchSupplierList();
+        }
+        else if (this.supplierRequestId == ACTION.FETCH_SUPPLIERS_BY_NAME)
+        {
+            this.fetchSupplierListByName();
+        }
+        else if (this.supplierRequestId == ACTION.FETCH_SUPPLIERS_BY_CELL)
+        {
+            this.fetchSupplierListByCell();
+        }
+        else if (this.supplierRequestId == ACTION.FETCH_SUPPLIERS_BY_EMAIL)
+        {
+            this.fetchSupplierListByEmail();
+        }
+    }
+    //---------------------------------supplier section ends ----------------------------------------//
 
 }
 
--- app/report/sales/salesordersummary.component.ts
+++ app/report/sales/salesordersummary.component.ts
@@ -4,6 +4,10 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {ModalDirective} from 'ngx-bootstrap';
 import {DTOSaleOrder} from './../../dto/DTOSaleOrder';
 import {EntitySaleOrder} from './../../dto/EntitySaleOrder';
+import {EntityUser} from './../../dto/EntityUser';
+import {EntityCustomer} from "./../../dto/EntityCustomer";
+import {EntityUserRole} from "./../../dto/EntityUserRole";
+import {DTOCustomer} from './../../dto/DTOCustomer';
 import {WebAPIService} from './../../webservice/web-api-service';
 import {PacketHeaderFactory} from './../../webservice/PacketHeaderFactory';
 import {ACTION} from './../../webservice/ACTION';
@@ -21,6 +25,8 @@ export class SalesOrderSummary {
     private datePipe: DatePipe;
     private webAPIService: WebAPIService;
     private subscribe: Subscription;
+    
+    @ViewChild('saleOrderSummaryCustomerModal') public saleOrderSummaryCustomerModal: ModalDirective;
 
     public showStartDatePicker: boolean = false;
     public showEndDatePicker: boolean = false;
@@ -30,6 +36,11 @@ export class SalesOrderSummary {
 
     private showNavBar: boolean = false;
     private activeMenu: string = "salesordersummary";
+    
+    private reqDTOCustomer: DTOCustomer;
+    private dtoCustomer: DTOCustomer;
+    private customerList: DTOCustomer[];
+    private customerRequestId: number;
     
     //private reqDTOSaleOrder: DTOSaleOrder;
     private saleOrderList: DTOSaleOrder[];
@@ -42,6 +53,10 @@ export class SalesOrderSummary {
     length = 0;
     pageSize = 10;
     pageSizeOptions = [5, 10];
+    
+    customerLength = 0;
+    customerPageSize = 10;
+    customerPageSizeOptions = [5, 10];
 
     constructor(private router: Router, public route: ActivatedRoute, webAPIService: WebAPIService, private navigationManager: NavigationManager, public datepipe: DatePipe) {
         this.navigationManager.showNavBarEmitter.subscribe((mode) => {
@@ -54,20 +69,28 @@ export class SalesOrderSummary {
                 this.activeMenu = menuName;
             }
         });
+        
         this.webAPIService = webAPIService;
         this.datePipe = datepipe;
-        //this.searchDTOCustomer = new DTOCustomer();
-        //this.searchDTOCustomer.entityUser = new EntityUser();
+
+        this.dtoCustomer = new DTOCustomer();
+        this.dtoCustomer.entityCustomer = new EntityCustomer();
+        this.dtoCustomer.entityUser = new EntityUser();
+        this.dtoCustomer.entityUserRole = new EntityUserRole();
         
-        //this.reqDTOSaleOrder = new DTOSaleOrder();
-        //this.reqDTOSaleOrder.entitySaleOrder = new EntitySaleOrder();
-        //this.reqDTOSaleOrder.offset = 0;
-        //this.reqDTOSaleOrder.limit = this.pageSize;
         this.totalSaleAmount = 0;
         this.offset = 0;
         this.limit = this.pageSize;
         this.saleOrderList = Array();
         this.fetchSaleOrderList();
+        
+        this.reqDTOCustomer = new DTOCustomer();
+        this.reqDTOCustomer.entityCustomer = new EntityCustomer();
+        this.reqDTOCustomer.entityUser = new EntityUser();
+        this.reqDTOCustomer.limit = this.customerPageSize;
+        this.reqDTOCustomer.offset = 0;
+        this.customerRequestId = ACTION.FETCH_CUSTOMERS;
+        this.fetchCustomerList();
     }
 
     ngOnInit() {
@@ -75,11 +98,106 @@ export class SalesOrderSummary {
         });
     }
     
+    public hideSaleOrderSummaryCustomerModal(): void {
+        this.saleOrderSummaryCustomerModal.hide();
+    }
+    
+    public showSaleOrderSummaryCustomerModal(event: Event) {
+        this.saleOrderSummaryCustomerModal.config.backdrop = false;
+        this.saleOrderSummaryCustomerModal.show();
+    }
+
+    
+    public searchSaleOrderSummaryCustomer(event: Event) {
+        this.reqDTOCustomer.limit = this.customerPageSize;
+        this.reqDTOCustomer.offset = 0;
+        if (this.reqDTOCustomer.entityCustomer.customerName != null && this.reqDTOCustomer.entityCustomer.customerName != "")
+        {
+            this.customerRequestId = ACTION.FETCH_CUSTOMERS_BY_NAME;
+            this.fetchCustomerListByName();
+        }
+        else if (this.reqDTOCustomer.entityCustomer.cell != null && this.reqDTOCustomer.entityCustomer.cell != "")
+        {
+            this.customerRequestId = ACTION.FETCH_CUSTOMERS_BY_CELL;
+            this.fetchCustomerListByCell();
+        }
+        else if (this.reqDTOCustomer.entityCustomer.email != null && this.reqDTOCustomer.entityCustomer.email != "")
+        {
+            this.customerRequestId = ACTION.FETCH_CUSTOMERS_BY_EMAIL;
+            this.fetchCustomerListByEmail();
+        }
+        else
+        {
+            //if nothing is set then get all customers
+            this.customerRequestId = ACTION.FETCH_CUSTOMERS;
+            this.fetchCustomerList();       
+        }
+    }
+    
+    public selectedCustomer(event: Event, customerId: number) {
+        this.saleOrderSummaryCustomerModal.hide();
+        let customerCounter: number;
+        for (customerCounter = 0; customerCounter < this.customerList.length; customerCounter++) {
+            if (this.customerList[customerCounter].entityCustomer.id == customerId) {
+                this.dtoCustomer = this.customerList[customerCounter];
+            }
+        }
+    }
+    
+    public fetchCustomerList() {
+        let requestBody: string = JSON.stringify(this.reqDTOCustomer);
+        this.webAPIService.getResponse(PacketHeaderFactory.getHeader(ACTION.FETCH_CUSTOMERS), requestBody).then(result => {
+            //console.log(result);
+            if (result.success && result.customers != null) {
+                this.customerList = result.customers;
+                this.customerLength = result.totalCustomers;
+            }
+        });
+    }
+    
+    public fetchCustomerListByName() {
+        let requestBody: string = JSON.stringify(this.reqDTOCustomer);
+        this.webAPIService.getResponse(PacketHeaderFactory.getHeader(ACTION.FETCH_CUSTOMERS_BY_NAME), requestBody).then(result => {
+            //console.log(result);
+            if (result.success && result.customers != null) {
+                this.customerList = result.customers;
+                this.customerLength = result.totalCustomers;
+            }
+        });
+    }
+    
+    public fetchCustomerListByCell() {
+        let requestBody: string = JSON.stringify(this.reqDTOCustomer);
+        this.webAPIService.getResponse(PacketHeaderFactory.getHeader(ACTION.FETCH_CUSTOMERS_BY_CELL), requestBody).then(result => {
+            if (result.success && result.customers != null) {
+                this.customerList = result.customers;
+                this.customerLength = result.totalCustomers;
+            }
+        });
+    }
+    
+    public fetchCustomerListByEmail() {
+        let requestBody: string = JSON.stringify(this.reqDTOCustomer);
+        this.webAPIService.getResponse(PacketHeaderFactory.getHeader(ACTION.FETCH_CUSTOMERS_BY_EMAIL), requestBody).then(result => {
+            if (result.success && result.customers != null) {
+                this.customerList = result.customers;
+                this.customerLength = result.totalCustomers;
+            }
+        });
+    }
+    
+    
+    
     public fetchSaleOrderList() {
         let fromDate: string = this.datepipe.transform(this.fromDate, 'yyyy-MM-dd');
         let toDate: string = this.datepipe.transform(this.toDate, 'yyyy-MM-dd');
         //let requestBody: string = JSON.stringify(this.reqDTOSaleOrder);
-        let requestBody: string = "{\"startDate\": \"" + fromDate + "\", \"endDate\": \"" + toDate + "\", \"offset\": \"" + this.offset + "\", \"limit\": \"" + this.limit + "\"}";
+        let userId: number = -1;
+        if (this.dtoCustomer.entityUser.id > 0)
+        {
+            userId = this.dtoCustomer.entityUser.id;
+        }
+        let requestBody: string = "{\"startDate\": \"" + fromDate + "\", \"endDate\": \"" + toDate + "\", \"userId\": \"" + userId + "\", \"offset\": \"" + this.offset + "\", \"limit\": \"" + this.limit + "\"}";
         this.webAPIService.getResponse(PacketHeaderFactory.getHeader(ACTION.FETCH_SALE_ORDER_SUMMARY), requestBody).then(result => {
             if (result.success && result.saleOrders != null) {
                 this.saleOrderList = result.saleOrders;
@@ -119,6 +237,27 @@ export class SalesOrderSummary {
 //    {
 //
 //    }
+    
+    onCustomerPaginateChange(event:PageEvent){
+        this.reqDTOCustomer.limit = event.pageSize;
+        this.reqDTOCustomer.offset = (event.pageIndex * event.pageSize) ;
+        if (this.customerRequestId == ACTION.FETCH_CUSTOMERS)
+        {
+            this.fetchCustomerList();
+        }
+        else if (this.customerRequestId == ACTION.FETCH_CUSTOMERS_BY_NAME)
+        {
+            this.fetchCustomerListByName();
+        }
+        else if (this.customerRequestId == ACTION.FETCH_CUSTOMERS_BY_CELL)
+        {
+            this.fetchCustomerListByCell();
+        }
+        else if (this.customerRequestId == ACTION.FETCH_CUSTOMERS_BY_EMAIL)
+        {
+            this.fetchCustomerListByEmail();
+        }
+    }
 
 }
 
